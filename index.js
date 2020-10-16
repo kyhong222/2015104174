de700b4e9b8bcb23bb2824669e36546d38851f65
junholee <juno7803@naver.com>
2020-10-16 11:24:31 +0900

    9단으로 수정

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-for(let i=1; i<=8; i++){
-  for(let j=1; j<=8; j++){
+for(let i=1; i<=9; i++){
+  for(let j=1; j<=9; j++){
     console.log(`${i}*${j}=${i*j}`);
   }
   console.log('');
 }
+// 9단으로 수정
